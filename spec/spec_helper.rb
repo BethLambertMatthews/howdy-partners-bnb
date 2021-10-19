c8de71157f93d4779a71bc43ea629bbf4a3ac4dc
Sam Thoburn <samuel.j.thoburn@outlook.com>
2021-10-19 12:46:51 +0100

    HP-config_02: Set up database connection class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,26 +32,4 @@ RSpec.configure do |config|
  
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-
-
- 
-  config.example_status_persistence_file_path = "spec/examples.txt"
-
- 
-  config.disable_monkey_patching!
-
-  config.warnings = true
-
-  
-  if config.files_to_run.one?
- 
-    config.default_formatter = "doc"
-  end
-
- 
-  config.profile_examples = 10
-
-  
-  config.order = :random
-
 end
