2aad090fc5b56810b9b6f2cf162cb67c1886b901
Steve Jones <steve.jones763@gmail.com>
2021-10-20 12:18:21 +0100

    HP-bugfix_01: Fixed property_spec test failing when database_connection_spec was also run due to leaking double.

--- spec/unit/database/database_connection_spec.rb
+++ spec/unit/database/database_connection_spec.rb
@@ -1,22 +1,22 @@
-# require 'database/database_connection'
+require 'database/database_connection'
 
-# describe DatabaseConnection do
-#   let(:connection) { double :connection }
+describe DatabaseConnection do
+  let(:connection) { double :connection }
 
-#   describe '#setup' do
-#     it 'sets up a connection to the database' do
-#       expect(PG).to receive(:connect).and_return(connection)
-#       DatabaseConnection.setup('howdy_partners_test')
-#     end
-#   end
+  describe '#setup' do
+    it 'sets up a connection to the database' do
+      expect(PG).to receive(:connect).and_return(connection)
+      DatabaseConnection.setup('howdy_partners_test')
+    end
+  end
 
-#   describe '#query' do
-#     it 'send a query to the PostgreSQL API' do
-#       expect(PG).to receive(:connect).and_return(connection)
-#       expect(connection).to receive(:exec_params).with('query', ['params'])
-#       DatabaseConnection.setup('howdy_partners_test')
-#       DatabaseConnection.query('query', ['params'])
-#     end
+  describe '#query' do
+    it 'sends a query to the postgres api' do
+      expect(PG).to receive(:connect).and_return(connection)
+      expect(connection).to receive(:exec_params).with("query", ['params'])
 
-#   end
-# end
+      DatabaseConnection.setup(connection)
+      DatabaseConnection.query("query", ['params'])
+    end
+  end
+end
